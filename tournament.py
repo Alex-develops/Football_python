7ea9630a9afea8863fb033992bf2002f0dd2ec0d
Alex_Sandra <alex.shestakova1@gmail.com>
2021-09-08 20:42:28 +0100

    initial commit

---
+++ tournament.py
@@ -0,0 +1,73 @@
+# Simulate a sports tournament
+
+import csv
+import sys
+import random
+
+# Number of simluations to run
+N = 1000
+
+
+def main():
+
+    # Ensure correct usage
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python tournament.py FILENAME")
+
+    teams = []
+    # Read data from csv file and add each team to the team list
+    filename = sys.argv[1]
+    with open(filename) as file:
+        reader = csv.DictReader(file)
+        # csv.DictReader gives an object that we can loop over and read the file one row at a time, treating each row as a dictionary.All read values are strings and we convert rating to int.
+        for team in reader:
+            team["rating"] = int(team["rating"])
+            teams.append(team)
+
+    counts = {}
+    # TODO: Simulate N tournaments and keep track of win counts
+    for i in range(N):
+        winner = simulate_tournament(teams)
+        if winner in counts:
+            counts[winner] += 1
+        else:
+            counts[winner] = 1
+
+    # Print each team's chances of winning, according to simulation
+    for team in sorted(counts, key=lambda team: counts[team], reverse=True):
+        print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
+
+
+def simulate_game(team1, team2):
+    """Simulate a game. Return True if team1 wins, False otherwise."""
+    rating1 = team1["rating"]
+    rating2 = team2["rating"]
+    probability = 1 / (1 + 10 ** ((rating2 - rating1) / 600))
+    return random.random() < probability
+
+
+def simulate_round(teams):
+    """Simulate a round. Return a list of winning teams."""
+    winners = []
+
+    # Simulate games for all pairs of teams
+    for i in range(0, len(teams), 2):
+        if simulate_game(teams[i], teams[i + 1]):
+            winners.append(teams[i])
+        else:
+            winners.append(teams[i + 1])
+
+    return winners
+
+
+def simulate_tournament(teams):
+    """Simulate a tournament. Return name of winning team."""
+    # Keep track of the winning counts in the winning dictionary
+
+    while len(teams) > 1:
+        teams = simulate_round(teams)
+    return teams[0]["team"]
+
+
+if __name__ == "__main__":
+    main()
